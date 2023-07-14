1634a8e6aa5b3cf93a1d3b9b6fa2b4a8c64417f8
P-Yuva <yugandharpadiri1005@gmail.com>
2023-07-14 09:56:15 +0000

    comit

--- queries.sql
+++ queries.sql
@@ -1,3 +1,4 @@
 --SQL query to find total count of female participated in each year
 
-select year,count(cand_sex) from election where cand_sex='F';
+select year,count(cand_sex) from election where cand_sex='F'
+group by year;

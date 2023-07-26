e5fd417402e49953f24407de890c1ce217f4f813
Lohith01-varada <vlohithreddy01@gmail.com>
2023-07-26 14:25:01 +0000

    COMMITTED TO MY BRANCH

--- mycode.sql
+++ mycode.sql
@@ -1,28 +1,32 @@
-select year,count(*) as female
+CREATE INDEX INDEX_EL ON ELECTION(YEAR);
+
+SET TIMING ON;
+--query 1
+select year,count(*) as TOTAL_FEMALE_CANDIDATE
 from election where CAND_SEX='F'
 group by YEAR;
 
-
-select year,ST_NAME,count(*) as total
+--query 2
+select year,ST_NAME,count(*) as TOTAL_CANDIDATES
 from election 
 group by YEAR,ST_NAME;
 
-
-select ST_NAME,year,PARTYNAME,sum(TOTVOTPOLL) as total
+--query 3
+select year,sum(TOTVOTPOLL) as TOTAL_VOTES
 from election 
-group by ST_NAME,year,PARTYNAME
-HAVING YEAR=1987 and PARTYNAME='BJP';
+where YEAR=1987 and PARTYNAME='BJP'
+group by year;
 
-
-select ST_NAME,COUNT(*) as total
+--query 4
+select ST_NAME,COUNT(*) as TOTAL_CANDIDATES
 from election 
 where YEAR=2004
 group by ST_NAME;
 
-\
-select top 5 PARTYNAME,YEAR,ST_NAME,SUM(TOTVOTPOLL) AS TOTAL
-from election 
-GROUP BY PARTYNAME,YEAR,ST_NAME
-having YEAR=2014 and ST_NAME='UTTAR PRADESH'
-ORDER BY TOTAL DESC;
-\
+--query 5
+SELECT PARTYNAME 
+FROM (SELECT PARTYNAME, SUM(TOTVOTPOLL) AS VOTES
+      FROM ELECTION WHERE YEAR=2014 AND ST_NAME='Uttar Pradesh'
+      GROUP BY PARTYNAME
+    )ORDER BY VOTES DESC
+    FETCH FIRST 5 ROWS ONLY;

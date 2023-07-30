7a67ff2071c181d0d94338d76094d07234ace652
P-Yuva <yugandharpadiri1005@gmail.com>
2023-07-30 05:51:15 +0000

    latest commit

--- YUGANDHAR_P.sql
+++ YUGANDHAR_P.sql
@@ -1,32 +1,31 @@
 
 set timing on;
 
---SQL query to find total count of female participated in each year
+--1
 
  select year,count(*)
  from election where cand_sex='F'group by year;
 
---2 total candidates who  paricipated in election at each state and year
+--2 
 select st_name ,year,count(*)
 from election 
 group by year,st_name
-order by st_name,year;
+order by year;
 
---3 sql query to find total votes BJP got in each state in the year of 1987
+--3 
 
 select st_name as state ,sum(totvotpoll) as Total_votes 
 from election where partyname='BJP' and year=1987
 group by st_name;
 
 
---4   sql query to find total candidates who participated in the election in each state in the year of 2004
+--4  
 select st_name, count(*)
 from election where year=2004
 group by st_name
 order by st_name asc;
 
-/*5 write a sql query to what are the top 5 parties theat get the most votes
-in uttur pradesh in the year of 2014*/
+--5 
 
 SELECT PARTYNAME 
 FROM (SELECT PARTYNAME, SUM(TOTVOTPOLL) AS VOTES

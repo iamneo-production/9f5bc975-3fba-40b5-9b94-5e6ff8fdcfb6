eef582f3f30a6740630536bf0b9dc0f4a07c03a9
PolinaNagaVineetha <vineethapolina@gmail.com>
2023-07-31 13:38:13 +0000

    COMMIT

--- Nagavineetha.sql
+++ Nagavineetha.sql
@@ -1,28 +1,27 @@
---CREATE INDEX INDEX_EL ON ELECTION(YEAR);
 
 SET TIMING ON;
 
 --1
- select year,count(*) as TOTAL_FEMALE_CANDIDATES
+ SELECT year,count(*) as TOTAL_FEMALE_CANDIDATES
 from  election where CAND_SEX='F'
 group  by YEAR;  
  
 
 --2   
- select  year,ST_NAME,count(*) as TOTAL_CANDIDATES
+SELECT  year,ST_NAME,count(*) as TOTAL_CANDIDATES
 from   election 
 group  by YEAR,ST_NAME;
 
 
 --3
- select year, sum(TOTVOTPOLL) as TOTAL_VOTES
+ SELECT year, sum(TOTVOTPOLL) as TOTAL_VOTES
 from  election 
 where  YEAR=1987 and PARTYNAME='BJP'
 group by year;
 
 
 --4
-select  ST_NAME, COUNT(*) as TOTAL_CANDIDATES
+SELECT  ST_NAME, COUNT(*) as TOTAL_CANDIDATES
  from  election 
 where YEAR=2004
 group by ST_NAME;

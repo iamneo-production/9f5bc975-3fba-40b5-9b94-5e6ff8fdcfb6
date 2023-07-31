75a5a0762783f5eb6a44ddc2bd5d4325ce6ee359
P-Yuva <yugandharpadiri1005@gmail.com>
2023-07-31 14:45:52 +0000

    final

--- YUGANDHAR_P.sql
+++ YUGANDHAR_P.sql
@@ -5,19 +5,19 @@ set timing on;
  select year,count(*)
  from election where cand_sex='F'group by year;
 
---2 total candidates who  paricipated in election at each state and year
+--2  total candidates who  paricipated in election at each state and year
 select st_name ,year,count(*)
 from election 
 group by year,st_name;
  
---3 sql query to find total votes BJP got in each state in the year of 1987
+--3  sql query to find total votes BJP got in each state in the year of 1987
 
 select st_name as state ,sum(totvotpoll) as Total_votes 
 from election where partyname='BJP' and year=1987
 group by st_name;
 
 
---4   sql query to find total candidates who participated in the election in each state in the year of 2004 
+--4    sql query to find total candidates who participated in the election in each state in the year of 2004 
 select st_name, count(*)
 from election where year=2004
 group by st_name

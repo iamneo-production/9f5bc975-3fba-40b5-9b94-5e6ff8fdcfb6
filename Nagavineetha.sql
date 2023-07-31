e99fcbfcdd73c4b9e5f22add6e85a2467b7a85a8
PolinaNagaVineetha <vineethapolina@gmail.com>
2023-07-31 12:33:58 +0000

    tunning

---
+++ Nagavineetha.sql
@@ -0,0 +1,33 @@
+--CREATE INDEX INDEX_EL ON ELECTION(YEAR);
+
+SET TIMING ON;
+
+--question 1
+select year,count(*) as TOTAL_FEMALE_CANDIDATES
+from election where CAND_SEX='F'
+group by YEAR;  
+
+--question 2   
+select year,ST_NAME,count(*) as TOTAL_CANDIDATES
+from election 
+group by YEAR,ST_NAME;
+
+--question 3
+select year,sum(TOTVOTPOLL) as TOTAL_VOTES
+from election 
+where YEAR=1987 and PARTYNAME='BJP'
+group by year;
+
+--question 4
+select ST_NAME,COUNT(*) as TOTAL_CANDIDATES
+from election 
+where YEAR=2004
+group by ST_NAME;
+
+--question 5
+SELECT PARTYNAME 
+FROM (SELECT PARTYNAME, SUM(TOTVOTPOLL) AS TOTAL_VOTES
+      FROM ELECTION WHERE YEAR=2014 AND ST_NAME='Uttar Pradesh'
+      GROUP BY PARTYNAME
+    )ORDER BY TOTAL_VOTES DESC
+    FETCH FIRST 5 ROWS ONLY;

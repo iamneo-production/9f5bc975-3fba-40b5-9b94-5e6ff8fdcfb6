41b3e4ad9e6ea711e0e3d1cc9a6a48317c1cbeea
Paladugu-Vinod <vinodpaladugu53@gmail.com>
2023-07-31 11:00:07 +0000

    Commiting Changes

--- Paladugu_Vinod.sql
+++ Paladugu_Vinod.sql
@@ -1,31 +1,31 @@
 
 SET TIMING ON;
---query 1
-select year,count(*) as TOTAL_FEMALE_CANDIDATE
-from election where CAND_SEX='F'
-group by year;
+--query:1
+SELECT year,count(*) as TOTAL_FEMALE_CANDIDATES
+from Election where CAND_SEX='F'
+group by year
+order by year ASC;
 
---query 2
-select year,ST_NAME,count(*) as total
-from election 
-group by YEAR,ST_NAME;
+--query:2
+SELECT year,ST_NAME,count(*) as Total
+from Election 
+group by year,ST_NAME;
 
---query 3
-select year,sum(TOTVOTPOLL) as total
-from election 
+--query:3
+SELECT ST_NAME,sum(TOTVOTPOLL) as Total_Votes
+from Election 
 where YEAR=1987 and PARTYNAME='BJP'
-group by year;
-
---query 4
-select ST_NAME,COUNT(*) as total
-from election 
-where YEAR=2004
 group by ST_NAME;
 
---query 5
-SELECT PARTYNAME 
-FROM (SELECT PARTYNAME, SUM(TOTVOTPOLL) AS VOTES
-      FROM ELECTION WHERE YEAR=2014 AND ST_NAME='Uttar Pradesh'
-      GROUP BY PARTYNAME
-    )ORDER BY VOTES DESC
-    FETCH FIRST 5 ROWS ONLY;
+--query:4
+SELECT ST_NAME,COUNT(*) as Total_Candidates
+from Election 
+where YEAR=2004
+group by ST_NAME
+order by ST_NAME ASC;
+
+--query:5
+SELECT PARTYNAME from (SELECT PARTYNAME, SUM(TOTVOTPOLL) AS VOTES 
+from Election WHERE YEAR=2014 AND ST_NAME='Uttar Pradesh'
+GROUP BY PARTYNAME) ORDER BY VOTES DESC
+FETCH FIRST 5 ROWS only;

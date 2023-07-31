e24787cdafe9040306eba8af2c06194a6a4ccb83
PolinaNagaVineetha <vineethapolina@gmail.com>
2023-07-31 13:23:05 +0000

    commit

--- Nagavineetha.sql
+++ Nagavineetha.sql
@@ -2,29 +2,29 @@
 
 SET TIMING ON;
 
---question 1
+--1
 select year,count(*) as TOTAL_FEMALE_CANDIDATES
 from election where CAND_SEX='F'
 group by YEAR;  
 
---question 2   
+--2   
 select year,ST_NAME,count(*) as TOTAL_CANDIDATES
 from election 
 group by YEAR,ST_NAME;
 
---question 3
+--3
 select year,sum(TOTVOTPOLL) as TOTAL_VOTES
 from election 
 where YEAR=1987 and PARTYNAME='BJP'
 group by year;
 
---question 4
+--4
 select ST_NAME,COUNT(*) as TOTAL_CANDIDATES
 from election 
 where YEAR=2004
 group by ST_NAME;
 
---question 5
+--5
 SELECT PARTYNAME 
 FROM (SELECT PARTYNAME, SUM(TOTVOTPOLL) AS TOTAL_VOTES
       FROM ELECTION WHERE YEAR=2014 AND ST_NAME='Uttar Pradesh'
